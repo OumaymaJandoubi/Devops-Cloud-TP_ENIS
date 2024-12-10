f610cca0ed2d3e7fe2df78e428e1a1d2326a135d
OumaymaJandoubi <jandoubi.omaima@enis.tn>
2024-12-10 14:12:17 +0100

    second commit

--- enis-app-tp/backend/backend/settings.py
+++ enis-app-tp/backend/backend/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'NAME': 'enis_tp',
         'USER': 'dbuser',
         'PASSWORD': 'DBpassword2024',
-        'HOST': 'mydb.czsmiu02gzxg.us-east-1.rds.amazonaws.com',
+        'HOST': 'DB_HOST',
         'PORT': 3306,
     }
 }
